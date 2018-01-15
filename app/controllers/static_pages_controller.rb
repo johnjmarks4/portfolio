86b4d69d6c5b85dcfcf2e211976ad732e6d458f8
John Marks <johnjmarks4@gmail.com>
2018-01-14 20:30:36 -0600

    Fill out skills page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,6 +7,10 @@ class StaticPagesController < ApplicationController
   end
 
   def skills
+    @skills = ["ruby on rails", "javascript", "mysql",
+               "css3", "object-oriented programming",
+               "bootstrap", "git", "cs fundamentals",
+               "postgresql", "jquery", "html5", "tdd"]
   end
 
   def my_work
